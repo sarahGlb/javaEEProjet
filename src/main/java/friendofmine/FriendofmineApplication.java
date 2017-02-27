c57b6aa022de22739563f035669bfeb4125667a7
sarahGlb <sarah-guilbert44@laposte.net>
2017-02-27 16:13:28 +0100

    first commit spring initialir

---
+++ src/main/java/friendofmine/FriendofmineApplication.java
@@ -0,0 +1,12 @@
+package friendofmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendofmineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendofmineApplication.class, args);
+	}
+}
